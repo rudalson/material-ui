0ceb80b6067a7610a62f60860efd96b069cba787
rudalson <rudalson@gmail.com>
2022-12-04 19:52:06 +0900

    ✨ theme 적용 시작

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { Button, Typography } from '@mui/material';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Typography variant='h4'>This is my app</Typography>
+      <Typography>Hit the like button</Typography>
+      <Button color='primary' variant='contained'>
+        Hello MUI v5
+      </Button>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { ThemeProvider, createTheme } from '@mui/material';
+
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#CCC',
+    },
+  },
+});
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
